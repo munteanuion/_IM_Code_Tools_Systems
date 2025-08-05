440c518ffb4e1e42207fe3600cb632c9e1803b34
munteanuion <71140604+munteanuion@users.noreply.github.com>
2025-08-06 00:05:18 +0300

    Add ParticleSystem container and array extensions
    Introduces ParticleSystemContainer class for managing arrays of ParticleSystems and ParticleSystemArrayExtensions for batch operations such as PlayAll, StopAll, ClearAll, and AnyPlaying. These utilities simplify handling multiple ParticleSystems in Unity projects.

---
+++ Runtime/UnityIteratorComponentContainers/ParticleSystemArrayExtensions.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace UnityIteratorComponentContainers
+{
+    public static class ParticleSystemArrayExtensions
+    {
+        public static void PlayAll(this ParticleSystem[] systems, bool withChildren = true)
+        {
+            foreach (var ps in systems)
+            {
+                if (ps != null)
+                    ps.Play(withChildren);
+            }
+        }
+
+        public static void StopAll(this ParticleSystem[] systems, bool withChildren = true)
+        {
+            foreach (var ps in systems)
+            {
+                if (ps != null)
+                    ps.Stop(withChildren);
+            }
+        }
+
+        public static void ClearAll(this ParticleSystem[] systems, bool withChildren = true)
+        {
+            foreach (var ps in systems)
+            {
+                if (ps != null)
+                    ps.Clear(withChildren);
+            }
+        }
+
+        public static bool AnyPlaying(this ParticleSystem[] systems)
+        {
+            foreach (var ps in systems)
+            {
+                if (ps != null && ps.isPlaying)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
