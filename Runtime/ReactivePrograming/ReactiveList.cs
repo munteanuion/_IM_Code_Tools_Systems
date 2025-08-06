ed02e3a8ad51350876ca97e242db59bba336b449
munteanuion <71140604+munteanuion@users.noreply.github.com>
2025-08-06 23:28:55 +0300

    Refactor ReactiveList to use Subscriber pattern
    Replaces event-based add/remove notifications in ReactiveList with a custom Subscriber<T> pattern, allowing for more flexible subscription management. Adds support for subscribing/unsubscribing to add/remove events, invoking with existing elements, and proper disposal. Introduces a generic Subscriber<T> class for single-argument actions.

--- Runtime/ReactivePrograming/ReactiveList.cs
+++ Runtime/ReactivePrograming/ReactiveList.cs
@@ -3,26 +3,102 @@ using System.Collections.Generic;
 
 namespace ReactivePrograming
 {
-    public class ReactiveList<T>
+    public class ReactiveList<T> : IDisposable
     {
-        public event Action<T> OnAdded;
-        public event Action<T> OnRemoved;
+        private readonly List<T> _elements = new();
         public IReadOnlyList<T> Elements => _elements;
-        
-        private List<T> _elements = new();
+
+        private readonly List<Subscriber<T>> _onAddSubscribers = new();
+        private readonly List<Subscriber<T>> _toAddAddSubscribers = new();
+        private readonly List<Subscriber<T>> _toRemoveAddSubscribers = new();
+
+        private readonly List<Subscriber<T>> _onRemoveSubscribers = new();
+        private readonly List<Subscriber<T>> _toAddRemoveSubscribers = new();
+        private readonly List<Subscriber<T>> _toRemoveRemoveSubscribers = new();
 
         
         
         public virtual void Add(T element)
         {
             _elements.Add(element);
-            OnAdded?.Invoke(element);
+            Invoke(_onAddSubscribers, _toAddAddSubscribers, _toRemoveAddSubscribers, element);
         }
 
         public virtual void Remove(T element)
         {
-            _elements.Remove(element);
-            OnRemoved?.Invoke(element);
+            if (_elements.Remove(element))
+                Invoke(_onRemoveSubscribers, _toAddRemoveSubscribers, _toRemoveRemoveSubscribers, element);
+        }
+
+        public IDisposable SubscribeOnAdd(Action<T> action, bool invokeWithAllExisting = false)
+        {
+            var subscriber = new Subscriber<T>(action, s => _toRemoveAddSubscribers.Add(s));
+            _toAddAddSubscribers.Add(subscriber);
+
+            if (invokeWithAllExisting)
+            {
+                foreach (var item in _elements)
+                    action(item);
+            }
+
+            return subscriber;
+        }
+
+        public IDisposable SubscribeOnRemove(Action<T> action)
+        {
+            var subscriber = new Subscriber<T>(action, s => _toRemoveRemoveSubscribers.Add(s));
+            _toAddRemoveSubscribers.Add(subscriber);
+            return subscriber;
+        }
+
+        public void Unsubscribe(IDisposable subscription) => subscription.Dispose();
+
+        public void UnsubscribeAll()
+        {
+            FlushSubscribers();
+            for (int i = _onAddSubscribers.Count - 1; i >= 0; i--)
+                _onAddSubscribers[i].Dispose();
+            for (int i = _onRemoveSubscribers.Count - 1; i >= 0; i--)
+                _onRemoveSubscribers[i].Dispose();
+            FlushSubscribers();
+        }
+
+        public void Dispose() => UnsubscribeAll();
+
+
+
+        private void Invoke(
+            List<Subscriber<T>> subscribers,
+            List<Subscriber<T>> toAdd,
+            List<Subscriber<T>> toRemove,
+            T element)
+        {
+            Flush(subscribers, toAdd, toRemove);
+
+            foreach (var subscriber in subscribers)
+                subscriber.Invoke(element);
+        }
+
+        private void FlushSubscribers()
+        {
+            Flush(_onAddSubscribers, _toAddAddSubscribers, _toRemoveAddSubscribers);
+            Flush(_onRemoveSubscribers, _toAddRemoveSubscribers, _toRemoveRemoveSubscribers);
+        }
+
+        private void Flush(List<Subscriber<T>> main, List<Subscriber<T>> toAdd, List<Subscriber<T>> toRemove)
+        {
+            if (toAdd.Count > 0)
+            {
+                main.AddRange(toAdd);
+                toAdd.Clear();
+            }
+
+            if (toRemove.Count > 0)
+            {
+                foreach (var subscriber in toRemove)
+                    main.Remove(subscriber);
+                toRemove.Clear();
+            }
         }
     }
 }
--- Runtime/ReactivePrograming/Subscriber.cs
+++ Runtime/ReactivePrograming/Subscriber.cs
@@ -6,8 +6,7 @@ namespace ReactivePrograming
     {
         private Action<T, K> _action;
         private Action<Subscriber<T,K>> _onDispose;
-
-
+        
 
         public Subscriber(Action<T, K> action, Action<Subscriber<T,K>> onDispose)
         {
@@ -16,15 +15,36 @@ namespace ReactivePrograming
         }
         
         
-        
+        public void Invoke(T arg1, K arg2) => _action?.Invoke(arg1, arg2);
         public void Dispose()
         {
             _onDispose?.Invoke(this);
             _action = null;
             _onDispose = null;
         }
+    }
 
+    
+    
+    public class Subscriber<T> : IDisposable
+    {
+        private Action<T> _action;
+        private Action<Subscriber<T>> _onDispose;
 
-        public void Invoke(T arg1, K arg2) => _action?.Invoke(arg1, arg2);
+        
+        public Subscriber(Action<T> action, Action<Subscriber<T>> onDispose)
+        {
+            _action = action;
+            _onDispose = onDispose;
+        }
+
+        
+        public void Invoke(T value) => _action?.Invoke(value);
+        public void Dispose()
+        {
+            _onDispose?.Invoke(this);
+            _action = null;
+            _onDispose = null;
+        }
     }
 }
