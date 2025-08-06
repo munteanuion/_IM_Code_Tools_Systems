37f4440b573031053001d24f24ff117188f74f00
munteanuion <71140604+munteanuion@users.noreply.github.com>
2025-08-06 23:09:08 +0300

    Add SpriteChanger UI system scripts
    Introduces SpriteChangerFromRefUI MonoBehaviour and SpriteChangerSettingsUI ScriptableObject for managing and applying sprite, aspect, and scale settings to Unity UI Images. Includes associated meta files and a new folder for organization.

---
+++ Runtime/_SpriteScriptablesSystem/SpriteChangerFromRefUI.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.__Project.Scripts.UI
+{
+    public class SpriteChangerFromRefUI : MonoBehaviour
+    {
+        public Image Image;
+        public SpriteChangerSettingsUI SpriteData;
+        public bool RuntimeSetOnStart = true;
+
+        
+
+        private void OnValidate()
+        {
+            Image ??= GetComponent<Image>();
+            
+            SetSpriteData();
+        } 
+
+        private void SetSpriteData()
+        {
+            if (SpriteData && Image)
+            {
+                Image.sprite = SpriteData.Sprite;
+                if (SpriteData.SetPreserveAspect) 
+                    Image.preserveAspect = SpriteData.PreserveAspect;
+                if (SpriteData.SetScale) 
+                    Image.transform.localScale = SpriteData.ScaleTransform;
+            }
+            else
+            {
+                Debug.LogWarning("SpriteChangerFromRefUI::SetSpriteData: SpriteData is null or Image is null");
+            }
+        }
+
+        private void Start()
+        {
+            if (RuntimeSetOnStart)
+            {
+                SetSpriteData();
+            }
+        }
+    }
+}
---
+++ Runtime/_SpriteScriptablesSystem/SpriteChangerSettingsUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Assets.__Project.Scripts.UI
+{
+    [CreateAssetMenu(fileName = "SpriteDataUI", menuName = "Scriptables/Sprite Changer", order = 0)]
+    public class SpriteChangerSettingsUI : ScriptableObject
+    {
+        [Header("Main Settings")]
+        public Sprite Sprite;
+        [Header("Additional Settings")]
+        public bool SetPreserveAspect = false;
+        public bool PreserveAspect = true;
+        [Header("Additional Scale Settings")]
+        public bool SetScale = false;
+        public Vector3 ScaleTransform = new Vector3(1, 1, 1);
+    }
+}
