c75fbedf11b837eb450da7a89b1e290dbf123161
Ion Munteanu <71140604+munteanuion@users.noreply.github.com>
2026-04-07 17:33:05 +0300

    Update LoggerExtensions.cs

--- Runtime/CodeExtensions/LoggerExtensions.cs
+++ Runtime/CodeExtensions/LoggerExtensions.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 namespace CodeExtensions
 {
     /*public static class LoggerExtensions
-    {
-        public static void Log(this Object loggerObject, string message)
-        {
-            Debug.Log($"<color=yellow>[{loggerObject.GetType()}]<color=white> {message}");
-        }
-        
-        public static void LogError(this Object loggerObject, string message)
-        {
-            Debug.LogError($"<color=red>[{loggerObject.GetType()}]<color=white> {message}");
-        }
-    }*/
+          {
+              public static void Log(this Object loggerObject, string message)
+              {
+                  Debug.Log($"<color=yellow>{GetLoggerPrefix(loggerObject)}<color=white> {message}");
+              }
+              
+              public static void LogError(this Object loggerObject, string message)
+              {
+                  Debug.LogError($"<color=red>{GetLoggerPrefix(loggerObject)}<color=white> {message}");
+              }
+      
+              private static string GetLoggerPrefix(Object loggerObject)
+              {
+                  var typePart = $"[{loggerObject.GetType()}]";
+      
+                  if (loggerObject is not MonoBehaviour monoBehaviour)
+                  {
+                      return typePart;
+                  }
+      
+                  var sceneName = monoBehaviour.gameObject.scene.name;
+                  var objectName = monoBehaviour.name;
+      
+                  return $"{typePart}[{sceneName}][{objectName}]";
+              }
+          }*/
 }
