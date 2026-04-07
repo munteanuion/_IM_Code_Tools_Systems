206e564c91abf1cfa9a636e09e491bc7d4fe93a7
Ion Munteanu <71140604+munteanuion@users.noreply.github.com>
2026-04-07 17:27:36 +0300

    .

---
+++ Runtime/CodeExtensions/LoggerExtensions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace CodeExtensions
+{
+    /*public static class LoggerExtensions
+    {
+        public static void Log(this Object loggerObject, string message)
+        {
+            Debug.Log($"<color=yellow>[{loggerObject.GetType()}]<color=white> {message}");
+        }
+        
+        public static void LogError(this Object loggerObject, string message)
+        {
+            Debug.LogError($"<color=red>[{loggerObject.GetType()}]<color=white> {message}");
+        }
+    }*/
+}
